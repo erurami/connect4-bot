3d5a209adecf548c6dffa4ce0fe8c0dc07699527
erurami <88187787+erurami@users.noreply.github.com>
2022-02-09 20:34:56 +0700

    Engine : Add headers

---
+++ engine.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#define INCLUDE_4INAROW
+#include "game/minigames.hpp"
+
+int ChooseNext(FourInARow::Game *pGameBoard);
+
+#include "engine.ipp"
+
---
+++ engine.ipp
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include "engine.hpp"
+
+
+int ChooseNext(FourInARow::Game *pGameBoard)
+{
+}
+
